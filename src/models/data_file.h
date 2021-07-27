12834a6bd39f121c8f3c64b921afb1bc8c76d878
darkvictor13 <01victoralmeida@gmail.com>
2021-07-26 23:21:37 -0300

    fim

--- src/models/data_file.h
+++ src/models/data_file.h
@@ -18,17 +18,26 @@
 
 #include "product.h"
 
+/**
+ * @brief Cabeçalho do arquivo de dados
+ */
 typedef struct {
     int regLast;
     int regFree;
 }DataHead;
 
+/**
+ * @brief acessos a elementos especificos da estrutura DataHead
+ */
 enum offsetHeadData {
     OFFSET_HEAD_DATA = 0,
 	OFFSET_LAST_DATA = offsetof(DataHead, regLast),
 	OFFSET_FREE_DATA = offsetof(DataHead, regFree)
 };
 
+/**
+ * @brief acessos a elementos especificos da estrutura Product
+ */
 enum offsetProductData {
     OFFSET_PRODUCT_CODE = offsetof(Product, code),
     OFFSET_PRODUCT_NAME = offsetof(Product, name),
@@ -37,28 +46,133 @@ enum offsetProductData {
     OFFSET_PRODUCT_LOCAL = offsetof(Product, local)
 };
 
+/**
+ * @brief cria um arquivo para o armazenamento dos dados dos produtos
+ * 
+ * @param dataFilePath cadeia de carateres contendo o nome e endereco do arquivo a ser criado
+ * @return FILE* 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 FILE *makeDataFile(char *dataFilePath);
 
-void writeDataHead(DataHead *dataHead, FILE *dataFile);
+/**
+ * @brief 
+ * 
+ * @param head 
+ * @param dataFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
+void writeDataHead(DataHead *head, FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param dataFile 
+ * @return DataHead* 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 DataHead *readDataHead(FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param value 
+ * @param offset 
+ * @param dataFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void writeDataHeadField(int value, int offset, FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param offset 
+ * @param dataFile 
+ * @return int 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 int readDataHeadField(int offset, FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param product 
+ * @param position 
+ * @param dataFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void writeDataRegistry(Product *product, int position, FILE *dataFile);
 
-Product* readDataRegistry(int position, FILE *dataFile);
+/**
+ * @brief 
+ * 
+ * @param position 
+ * @param dataFile 
+ * @return Product* 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
+Product *readDataRegistry(int position, FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param value 
+ * @param offset 
+ * @param position 
+ * @param dataFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void writeDataRegistryField(int value, int offset, int position, FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param offset 
+ * @param position 
+ * @param dataFile 
+ * @return int 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 int readDataRegistryField(int offset, int position, FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param position 
+ * @param dataFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void clearDataRegistry(int position, FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param product 
+ * @param dataFile 
+ * @return int 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 int insertDataRegistry(Product *product, FILE *dataFile);
 
+/**
+ * @brief 
+ * 
+ * @param position 
+ * @param dataFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void removeDataRegistry(int position, FILE *dataFile);
 
 #endif
--- src/models/index_file.h
+++ src/models/index_file.h
@@ -17,14 +17,23 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * @brief Ordem da árvore B
+ */
 #define ORDER 5 // greater than or equal to 5
 
+/**
+ * @brief Cabeçalho do arquivo de índices
+ */
 typedef struct {
     int regRoot;
     int regLast;
     int regFree;
 }IndexHead;
 
+/**
+ * @brief acessos a elementos especificos da estrutura IndexHead
+ */
 enum offsetHeadIndex {
     OFFSET_HEAD_INDEX = 0,
 	OFFSET_ROOT_INDEX = offsetof(IndexHead, regRoot),
@@ -32,6 +41,10 @@ enum offsetHeadIndex {
 	OFFSET_FREE_INDEX = offsetof(IndexHead, regFree)
 };
 
+/**
+ * @brief Um elemento de um registro com seus filhos
+ * no arquivo de índices
+ */
 typedef struct {
     int key;
     int position;
@@ -39,6 +52,9 @@ typedef struct {
     int rightChild;
 }RegistryField;
 
+/**
+ * @brief Registro de elementos da árvore B
+ */
 typedef struct {
     int numberOfKeys;
 	int key[ORDER - 1];
@@ -46,6 +62,9 @@ typedef struct {
 	int children[ORDER];
 }Registry;
 
+/**
+ * @brief acessos a elementos especificos da estrutura Registry
+ */
 enum offsetRegistryIndex {
     OFFSET_REGISTRY_NUM = offsetof(Registry, numberOfKeys),
     OFFSET_REGISTRY_KEYS = offsetof(Registry, key),
@@ -53,34 +72,169 @@ enum offsetRegistryIndex {
     OFFSET_REGISTRY_CHILDS = offsetof(Registry, children)
 };
 
+/**
+ * @brief 
+ * 
+ * @param indexFile 
+ * @return int 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 int isEmptyIndex(FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param indexFilePath 
+ * @return FILE* 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 FILE *makeIndexFile(char *indexFilePath);
 
+/**
+ * @brief Create a Registry object
+ * 
+ * @param key 
+ * @param position 
+ * @param leftChild 
+ * @param rightChild 
+ * @return Registry* 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 Registry *createRegistry(int key, int position, int leftChild, int rightChild);
 
+/**
+ * @brief Create a Registry Field object
+ * 
+ * @param key 
+ * @param position 
+ * @param leftChild 
+ * @param rightChild 
+ * @return RegistryField* 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 RegistryField *createRegistryField(int key, int position, int leftChild, int rightChild);
 
-void writeIndexHead(IndexHead *indexHead, FILE *indexFile);
+/**
+ * @brief 
+ * 
+ * @param head 
+ * @param indexFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
+void writeIndexHead(IndexHead *head, FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param indexFile 
+ * @return IndexHead* 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 IndexHead *readIndexHead(FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param value 
+ * @param offset 
+ * @param indexFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void writeIndexHeadField(int value, int offset, FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param offset 
+ * @param indexFile 
+ * @return int 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 int readIndexHeadField(int offset, FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param registry 
+ * @param position 
+ * @param indexFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void writeIndexRegistry(Registry *registry, int position, FILE *indexFile);
 
-Registry* readIndexRegistry(int position, FILE *indexFile);
+/**
+ * @brief 
+ * 
+ * @param position 
+ * @param indexFile 
+ * @return Registry* 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
+Registry *readIndexRegistry(int position, FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param value 
+ * @param offset 
+ * @param position 
+ * @param indexFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void writeIndexRegistryField(int value, int offset, int position, FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param offset 
+ * @param position 
+ * @param indexFile 
+ * @return int 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 int readIndexRegistryField(int offset, int position, FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param position 
+ * @param indexFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void clearIndexRegistry(int position, FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param registry 
+ * @param indexFile 
+ * @return int 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 int insertIndexRegistry(Registry *registry, FILE *indexFile);
 
+/**
+ * @brief 
+ * 
+ * @param position 
+ * @param indexFile 
+ * @pre Nenhuma
+ * @post Nenhuma
+ */
 void removeIndexRegistry(int position, FILE *indexFile);
 
 #endif
